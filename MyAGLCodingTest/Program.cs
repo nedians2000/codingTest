543978b5bff2f251557453a45b8ab7ed91e283fd
nedians2000 <nedians2000@yahoo.com>
2018-01-16 13:47:31 +1100

    Completed

---
+++ MyAGLCodingTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+    
+namespace MyAGLCodingTest
+{
+    public class JsonCats
+    {
+        public static void Main(string[] args)
+        {
+            DisplayCats();
+            Console.ReadLine();
+        }
+        public static void DisplayCats()
+        {
+            var azureJsonResult = GetJSONData().Result;
+
+            var myPets = JArray.Parse(azureJsonResult);
+
+            var myCats = myPets.GroupBy(
+                person => person["gender"].Value<string>(), // gender of pets
+                person => person["pets"].Where(pet => pet["type"].Value<string>() == "Cat").Select(pet => pet["name"].Value<string>()), // get the name of cats
+                (gender, cats) => new { Gender = gender, Cats = cats.SelectMany(cat => cat).OrderBy(name => name) }); // get cat list and sort names in segment
+
+            foreach (var cat in myCats)
+            {
+                Console.WriteLine(cat.Gender);
+                Console.WriteLine("................" + System.Environment.NewLine);
+
+                foreach (var catname in cat.Cats)
+                {
+                    Console.WriteLine(catname);
+
+                }
+                Console.WriteLine(System.Environment.NewLine);
+            }
+
+        }
+        private static async Task<string> GetJSONData()
+        {
+            var data = string.Empty;
+
+            using (var myClient = new HttpClient())
+            {
+                myClient.BaseAddress = new Uri("http://agl-developer-test.azurewebsites.net/");
+                myClient.DefaultRequestHeaders.Accept.Clear();
+                myClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await myClient.GetAsync("people.json");
+                if (response.IsSuccessStatusCode)
+                {
+                    data = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return data;
+        }
+    }
+}
---
+++ MyUnitTesting/AGLCodeUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyAGLCodingTest;
+
+namespace MyUnitTesting
+{
+    [TestClass]
+    public class AGLCodeUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            JsonCats.DisplayCats();
+        }
+    }
+}
